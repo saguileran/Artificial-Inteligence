6c212e4900adc211a61b6575c1fa610525636ab5
saguileran <saguileran@unal.edu.co>
2019-09-21 17:18:03 -0500

    Modifying script, adding class

--- Sensor.py
+++ Sensor.py
@@ -3,19 +3,18 @@ import matplotlib.pyplot as plt
 
 #------------Clase-----------
 class Sensor:
-  def __init__(self, x, y, z):
-    self.x = x
-    self.y = y
-    self.z = z
-    self.Amplitud = (x**2+y**2+z**2)**0.5
+  def __init__(self):
     self.X=[]; self.Y=[]; self.Z=[]; self.A=[]
-  def Actualizando(self):
-      self.X.append(self.x); self.Y.append(self.y);
-      self.Z.append(self.z); self.A.append(self.Amplitud)
 
-Acelerometro = Sensor(0,4,3)
-Acelerometro.X.append(2)
-print(Acelerometro.X)
+  def getX(self): return(self.X)
+  def getY(self): return(self.Y)
+  def getZ(self): return(self.Z)
+  def getA(self): return(self.A)
+
+  def Actualizando(self, x, y, z):
+      self.X.append(float(x)); self.Y.append(float(y));
+      self.Z.append(float(z)); self.A.append((float(x)**2+float(y)**2+float(z)**2)**0.5)
+###Se puede modificar la parte  de append para que las listas tengan una longitud constante###
 
 #-----------Creando email-----------
 def Email(body):
@@ -42,51 +41,44 @@ def Email(body):
    server.close()
    print ('Email sent!')
 
-#------------Funciones-------------------------
-def StrToFloat(A):
- for i in range(len(A)): A[i]=float(A[i])
-self.X=[
-def Magnitud(A): return(round((A[0]**2+A[1]**2+A[2]**2)**0.5,2))
-
 #-------------Creando conexion-------------------
-UDP_IP_ADDRESS = "192.168.1.7"
+UDP_IP_ADDRESS = "192.168.1.112"
 UDP_PORT_NO = 5550
 
 serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
 
 #--------------Tomando Datos---------------------------
-Acelerometro = Sensor(1,2,3)
-Grioscopio= Sensor(1,2,3)
-
-
-Tiempo, Acelerometro, Giroscopio, Magnetico, GPS = [],[],[],[],[] #Listas de posicion
-dt=0.01 #Distancia entre datos
+#dt=0.01 #Distancia entre datos
 tolacel, tolgir= 2, 20  #newtons, no se
 flat= False
-GPS=[0,0,0]
 
-for i in range(100):
+GPS = Sensor()
+Acelerometro = Sensor()
+Giroscopio = Sensor()
+
+for i in range(1000):
 
    data, addr = serverSock.recvfrom(1024)
    Data = data.decode("utf-8").split(",")
+   #print(Data)
 
-   StrToFloat(Data)
+   #Tiempo.append(float(Data[0]))
 
-   Tiempo.append(float(Data[0]))
-   if len(Data)>14 and Data[1]==1: GPS=Data[2:5]; k=1
+   if len(Data)>14 and Data[1]==' 1':    GPS.Actualizando(Data[2], Data[3], Data[4]);  k=1
    else: k=0
-   if Data[1+4*k]==3:   A=Data[2+4*k:5+4*k]; Acelerometro.append(Magnitud(A))
-   if len(Data)>5 and Data[5+4*k]==4:    A=Data[6+4*k:9+4*k]; Giroscopio.append(Magnitud(A))
-   #if len(Data)>10 and Data[9+4*k]==5:    A=Data[10+4*k:13+4*k]; Magnetico.append(Magnitud(A))
+   if Data[1+4*k]==' 3':   Acelerometro.Actualizando(Data[2+4*k], Data[3+4*k], Data[4+4*k])
+   if len(Data)>5 and Data[5+4*k]==4:    Grioscopio.Actualizando(Data[6+4*k], Data[7+4*k], Data[8+4*k])
+   #Sensor.Actualizando(Giroscopio)
+  #if len(Data)>10 and Data[9+4*k]==5:    A=Data[10+4*k:13+4*k]; Magnetico.append(Magnitud(A))
 
-   if i>0 and (Acelerometro[i]-Acelerometro[i-1]>tolacel) and len(GPS)!=0 and flat == False: # and #or Giroscopio[i]-Giroscopio[i-1]>tolgir) and
+#   if i>0 and (Acelerometro[i]-Acelerometro[i-1]>tolacel) and flat == False: # and #or Giroscopio[i]-Giroscopio[i-1]>tolgir) and
 #     print("se cayo", Data[0]);
-     Email("Se ha caido su abuelita en x = {}, y = {}, z = {} el dia {} a las {} horas".format(GPS[0], GPS[1], GPS[2],str(datetime.datetime.now().date()) , str(datetime.datetime.now().time())[:8]  ))
-     flat = True #Para no enviar mas correos
+#     Email("Se ha caido su abuelita en x = {}, y = {}, z = {} el dia {} a las {} horas".format(GPS[0], GPS[1], GPS[2],str(datetime.datetime.now().date()) , str(datetime.datetime.now().time())[:8]  ))
+#     flat = True #Para no enviar mas correos
 
-
-   print("Datos:", data, len(data))
+   print("Datos:", GPS.getX(), GPS.getY(), GPS.getZ())
+   print(" ")
 #   print(" ")
 #print("Tiempo: ", Tiempo);
 #print("Acelrometro: ", Acelerometro);
